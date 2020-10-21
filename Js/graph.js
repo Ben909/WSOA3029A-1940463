f9700f3299a45bfdbb6887d2ff81ab5e127c9965
WSOA3028_1940463 <55377439+Ben909@users.noreply.github.com>
2020-10-21 22:11:27 +0200

    made a few grammer corrections

--- Js/graph.js
+++ Js/graph.js
@@ -1,76 +1,60 @@
-const pollChartData = [
-    {
-        province: "Eastern Cape",
-        recovered: 155,
-        color: "rgb(255, 99, 132)"
-    },
-    {
-        province: "Free State",
-        recovered: 86,
-        color: "rgb(54, 162, 235)"
-    },
-    {
-        province: "Gauteng",
-        recovered: 120,
-        color: "rgb(36, 36, 36)"
-    },
-    {
-        province: "KwaZulu-Natal",
-        recovered: 100,
-        color: "rgb(255, 159, 64)"
-    },
-    {
-        province: "Limpopo",
-        recovered: 78,
-        color: "rgb(75, 192, 192)"
-    },
-    {
-        province: "Mpumalanga",
-        recovered: 134,
-        color: "rgb(25, 180, 86)"
-    },
-    {
-        province: "Northern Cape",
-        recovered: 180,
-        color: "rgb(83, 15, 155)"
-        
-    },
-    {
-        province: "North West",
-        recovered: 100,
-        color: "rgb(253, 202, 155)"
-        
-    },
-    {
-        province: "Western Cape",
-        recovered: 50,
-        color: "rgb(293, 83, 255)"
-        
-    },
-    {
-        province: "Northern Cape",
-        recovered: 64,
-        color: "rgb(117, 152, 175)"
-        
-    },
-    {
-        province: "Northern Cape",
-        recovered: 140,
-        color: "rgb(153, 102, 255)"
-        
-    },
-];
+const Mydata = [
+    { region: 'Eastern Cape', count: 86400 },
+    { region: 'Free State', count: 35537 },
+    { region: 'Gauteng', count: 199352 },
+    { region: 'KwaZulu-Natal', count: 111831 },
+    { region: 'Limpopo', count: 15326 },
+    { region: 'Mpumalanga', count: 27040 },
+    { region: 'North West', count: 28385 },
+    { region: 'Western Cape', count: 105347 },
+    { region: 'Northern Cape', count: 16356 },
+  ];
+  
 
-const ctx = document.getElementById('chart').getContext('2d');
-const pollChart = new Chart(ctx, {
-    type: 'bar',
-    data: {
-        labels: pollChartData.map(pollOption => pollOption.province),
-        datasets: [{
-            label: 'Total Recoveries',
-            data: pollChartData.map(pollOption => pollOption.recovered),
-            backgroundColor: pollChartData.map(pollOption => pollOption.color),
-            borderWidth: 6
-        }]
-    },
-});
+  const margin = { top: 50, bottom: 50, left: 50, right: 50 };
+  const height = 450;
+  const width = 900;
+
+  const svg = d3.select('#bar-3Dcontainer')
+    .append('svg')
+    .attr('width', width - margin.left - margin.right)
+    .attr('height', height - margin.top - margin.bottom)
+    .attr("viewBox", [0, 0, width, height]);
+  
+  const x = d3.scaleBand()
+    .domain(d3.range(Mydata.length))
+    .range([margin.left, width - margin.right])
+    .padding(0.1)
+  
+  const y = d3.scaleLinear()
+    .domain([0, 220000])
+    .range([height - margin.bottom, margin.top])
+  
+  svg
+    .append("g")
+    .attr("fill", 'green')
+    .selectAll("rect")
+    .data(Mydata.sort((a, b) => d3.descending(a.score, b.count)))
+    .join("rect")
+      .attr("x", (d, i) => x(i))
+      .attr("y", d => y(d.count))
+      .attr('title', (d) => d.count)
+      .attr("class", "rect")
+      .attr("height", d => y(0) - y(d.count))
+      .attr("width", x.bandwidth());
+  
+  function yAxis(g) {
+    g.attr("transform", `translate(${margin.left}, 0)`)
+      .call(d3.axisLeft(y).ticks(null, Mydata.format))
+      .attr("font-size", '20px')
+  }
+  
+  function xAxis(g) {
+    g.attr("transform", `translate(0,${height - margin.bottom})`)
+      .call(d3.axisBottom(x).tickFormat(i => Mydata[i].region))
+      .attr("font-size", '15px')
+  }
+  
+  svg.append("g").call(xAxis);
+  svg.append("g").call(yAxis);
+  svg.node();
